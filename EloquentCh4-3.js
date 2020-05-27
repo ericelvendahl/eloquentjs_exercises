ed767eedc46519c162e38c583462c3cb1883da08
“ericelvendahl” <ericelvendahl@gmail.com>
2020-05-27 04:54:32 -0500

    Started Chapter 4-3-1

--- EloquentCh4-3.js
+++ EloquentCh4-3.js
@@ -1,4 +1,14 @@
 //Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument.
+function arrayToList(arr) {
+  let list = null;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(i);
+    list = { value: arr[i], rest: list };
+  }
+  return list;
+}
+let x = arrayToList([5, 2, 1, 4]);
+console.log(x);
 
 //Also write a listToArray function that produces an array from a list.
 

2d42236506cb9309937321b248a7aec9c8dfaceb
“ericelvendahl” <ericelvendahl@gmail.com>
2020-05-27 03:42:57 -0500

    Further work on reversal

--- EloquentCh4-2.js
+++ EloquentCh4-2.js
@@ -1,27 +1,31 @@
 let reverseArray = (arr) => {
   let tempArray = [];
-  for (let i = arr.length; i >= 0; i--) {
+  for (let i = arr.length - 1; i >= 0; i--) {
     tempArray.push(arr[i]);
   }
   return tempArray;
 };
 
 let reverseArrayInPlace = (arr) => {
-  let tempArray = [];
-  for (let i = arr.length; i > 0; i--) {
-    tempArray.push(i);
-  }
+  let newArray = [];
+  let tempElement = "";
   for (let i = 0; i < arr.length; i++) {
-    arr[i] = tempArray[i];
+    tempElement = arr[i];
+    arr[i] = arr[arr.length - i];
+    arr[arr.length - i] = tempElement;
   }
+  return arr;
 };
 
 let myArray = [12, 15, 22, 7722, 3];
 //console.log(reverseArray(myArray));
 
-console.log("Here: " + reverseArray(["A", "B", "C"]));
+let myArrayHolder = reverseArray(["A", "B", "C"]);
+console.log(myArrayHolder);
+//console.log(`Here: ${reverseArray(["A", "B", "C"])}`);
 // → ["C", "B", "A"];
+
 let arrayValue = [1, 2, 3, 4, 5];
-reverseArrayInPlace(arrayValue);
+console.log(`This is reverseArrayInPlace: ${reverseArrayInPlace(arrayValue)}`);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
